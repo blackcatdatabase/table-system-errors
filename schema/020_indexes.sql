8b64b26d38b2e72269ce0cadeb465a6422d48a8f
Tolben <vitek.pasek@gmail.com>
2025-10-22 00:00:45 +0200

    chore(schema): add/update generated schema + README (umbrella sync)

--- schema/020_indexes.sql
+++ schema/020_indexes.sql
@@ -1,3 +1,3 @@
--- Auto-generated from schema-map.psd1 on 2025-10-21T02:32:05
+-- Auto-generated from schema-map.psd1 @ 1e83bb6 (2025-10-21T10:18:36+02:00)
 -- table: system_errors
 CREATE INDEX idx_system_errors_last_seen ON system_errors (last_seen);
--- schema/030_foreign_keys.sql
+++ schema/030_foreign_keys.sql
@@ -1,4 +1,4 @@
--- Auto-generated from schema-map.psd1 on 2025-10-21T02:32:05
+-- Auto-generated from schema-map.psd1 @ 1e83bb6 (2025-10-21T10:18:36+02:00)
 -- table: system_errors
 ALTER TABLE system_errors ADD CONSTRAINT fk_err_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET NULL;
 
